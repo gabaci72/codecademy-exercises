4630d0098259abe3fb12cdbaf28338e5638dadc1
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-23 17:50:40 +0200

    Loveseat project started

---
+++ 04-Python/03-LoveSeats/loveseats.py
@@ -0,0 +1,8 @@
+lovely_loveseat_description = """
+Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
+"""
+lovely_loveseat_price = 254.00
+stylish_settee_description = '''
+Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
+'''
+tylish_settee_price = 180.50
