951bcf15523f9da26c1a75b62cec6d615560dd99
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-24 18:09:47 +0200

    Clean the Data Exercises Started

---
+++ 04-Python/04-CleantheData/script.py
@@ -0,0 +1,16 @@
+import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import the CSV files and create the DataFrames:
+user_data = pd.read_csv("user_data.csv")
+pop_data = pd.read_csv("pop_data.csv")
+
+# Paste print code here:
+
+
+# Paste merge code here:
+
+
+# Paste location code here:
