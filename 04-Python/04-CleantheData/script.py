beb80c13af1d252c35ffad52210cdeb4b429ab2e
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-24 20:27:21 +0200

    following code to plot a histogram

--- 04-Python/04-CleantheData/script.py
+++ 04-Python/04-CleantheData/script.py
@@ -3,19 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Import the CSV file and create the DataFrames:
+# Import CSVs:
 user_data = pd.read_csv("user_data.csv")
 pop_data = pd.read_csv("pop_data.csv")
 
-# Paste print code here:
-print(user_data.head(15))
-
-# Paste merge code here:
+# Merged tables with location data:
 new_df = pd.merge(user_data, pop_data)
-print(new_df.head(15))
-
-# Paste location code here:
 new_df.loc[new_df.population_proper < 100000, "location"] = "rural"
 new_df.loc[new_df.population_proper >= 100000, "location"] = "urban"
 
-print(new_df.head(15))
+# Paste histogram code:
+age = new_df["age"]
+sns.displot(age)
+
+plt.show() 
+
+
+# Paste mean age location code:
+
+
+# Paste barplot code:
+
+
+# Paste violinplot code:
+
