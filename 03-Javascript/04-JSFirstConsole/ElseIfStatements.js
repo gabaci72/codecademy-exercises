89f0ff800f5a563e3f8b1dbf2a6c96d2a508e1ee
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-31 22:40:47 +0100

    first root code changing to else if statement

--- 03-Javascript/04-JSFirstConsole/ElseIfStatements.js
+++ 03-Javascript/04-JSFirstConsole/ElseIfStatements.js
@@ -2,7 +2,8 @@ let season = 'summer';
 
 if (season === 'spring') {
   console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
 } else {
   console.log('Invalid season.');
 }
-
