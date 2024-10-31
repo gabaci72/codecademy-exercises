1ffb58334d35600357d208161837ee9364ce85ce
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-31 12:53:15 +0100

    design nav bar and declare to var

---
+++ 03-Javascript/06-DogYears/index.js
@@ -0,0 +1,5 @@
+// Declare a constant variable for my age
+const myAge = 25; // Replace 25 with your actual age
+
+// Initialize a variable for the first two years of a dog's life
+let earlyYears = 2;
