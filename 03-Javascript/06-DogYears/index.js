20b08b10be5265d8c52bb7f7bac0e404b19b42f3
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-31 12:57:18 +0100

    multiply the value saved to earlyYea

--- 03-Javascript/06-DogYears/index.js
+++ 03-Javascript/06-DogYears/index.js
@@ -3,3 +3,6 @@ const myAge = 25; // Replace 25 with your actual age
 
 // Initialize a variable for the first two years of a dog's life
 let earlyYears = 2;
+
+/*Use the multiplication assignment operator to multiply the value saved to earlyYears by 10.5 and reassign it to earlyYears*/
+earlyYears *= 10.5;
