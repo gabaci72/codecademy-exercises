b570d2e9b1d46f982e24c7aa307a5e023fbe1dea
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-10-31 22:42:52 +0100

    This structure now includes an additional else if statement to handle the 'fall' season.

--- 03-Javascript/04-JSFirstConsole/ElseIfStatements.js
+++ 03-Javascript/04-JSFirstConsole/ElseIfStatements.js
@@ -4,6 +4,8 @@ if (season === 'spring') {
   console.log('It\'s spring! The trees are budding!');
 } else if (season === 'winter') {
   console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
 } else {
   console.log('Invalid season.');
 }
