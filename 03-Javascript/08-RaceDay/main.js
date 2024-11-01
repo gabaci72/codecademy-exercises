8fb2b40f6771080d0ac23d636ae8a06c6e35f8f1
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 13:54:18 +0100

    “Youth registrants run at 12:30 pm (regardless of registration)”

--- 03-Javascript/08-RaceDay/main.js
+++ 03-Javascript/08-RaceDay/main.js
@@ -10,4 +10,6 @@ if (age > 18 && registeredEarly) {
   console.log(`You will race at 9:30 am. Your race number is ${raceNumber}.`);
 } else if (age > 18 && !registeredEarly) {
   console.log(`You will race at 11:00 am. Your race number is ${raceNumber}.`);
+} else if (age < 18) {
+  console.log(`You will race at 12:30 pm. Your race number is ${raceNumber}.`);
 }
