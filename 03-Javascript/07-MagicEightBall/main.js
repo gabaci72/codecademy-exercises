4e307a175c3326ad2cf5d603943ebf604f3a876c
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 12:03:05 +0100

    This code checks if userName has a value and logs the appropriate greeting

--- 03-Javascript/07-MagicEightBall/main.js
+++ 03-Javascript/07-MagicEightBall/main.js
@@ -2,6 +2,9 @@
 
 The user will be able to input a question, then our program will output a random fortune.*/
 
-let userName = ''; // In the first line of the program, define a variable called userName that is set to an empty string.
+let userName = 'Seriano'; 
+// In the first line of the program, define a variable called userName that is set to an empty string.
 
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
+// This code checks if userName has a value and logs the appropriate greeting.
