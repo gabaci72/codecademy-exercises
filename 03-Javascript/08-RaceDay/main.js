79760674bac5226e4c11edae6869c7fc7625d797
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 13:15:14 +0100

    depending on the test condition

--- 03-Javascript/08-RaceDay/main.js
+++ 03-Javascript/08-RaceDay/main.js
@@ -1,1 +1,5 @@
+/*Race DayY
+Codecademy’s annual race is just around the corner! This year, we have a lot of participants. 
+ou have been hired to write a program that will register runners for the race and give them instructions on race day.*/
 let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = false; // or true, depending on the test condition
