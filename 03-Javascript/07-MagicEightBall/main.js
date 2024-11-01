f9d7591ad65e50cd27f8e05f178ccff32c6f91d9
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 12:52:39 +0100

    This switch statement checks the value of randomNumber and assigns the corresponding Magic Eight Ball phrase to eightBall

--- 03-Javascript/07-MagicEightBall/main.js
+++ 03-Javascript/07-MagicEightBall/main.js
@@ -20,7 +20,35 @@ let randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = ''; //Create one more variable named eightBall, and set it equal to an empty string
 
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = 'Error';
+}
 
-
+//This switch statement checks the value of randomNumber and assigns the corresponding Magic Eight Ball phrase to eightBall
 
 
