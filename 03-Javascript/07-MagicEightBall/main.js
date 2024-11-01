3798ba2a61238409c7c5eda4d25fd0b075a1c3c1
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 11:51:47 +0100

    define a variable called userName that is set to an empty string.

---
+++ 03-Javascript/07-MagicEightBall/main.js
@@ -0,0 +1,7 @@
+/*In this project we will build a Magic Eight Ball using control flow in JavaScript.
+
+The user will be able to input a question, then our program will output a random fortune.*/
+
+let userName = ''; // In the first line of the program, define a variable called userName that is set to an empty string.
+
+
