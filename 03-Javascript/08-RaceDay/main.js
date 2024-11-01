6a27e1706d90f8fff5de2629255aa1a07446bb55
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 13:47:18 +0100

    Write the corresponding else if statement.

--- 03-Javascript/08-RaceDay/main.js
+++ 03-Javascript/08-RaceDay/main.js
@@ -1,6 +1,3 @@
-/*Race DayY
-Codecademy’s annual race is just around the corner! This year, we have a lot of participants. 
-ou have been hired to write a program that will register runners for the race and give them instructions on race day.*/
 let raceNumber = Math.floor(Math.random() * 1000);
 const registeredEarly = false; // or true, depending on the test condition
 const age = 19; // Set to the runner's age
@@ -8,9 +5,9 @@ const age = 19; // Set to the runner's age
 if (age > 18 && registeredEarly) {
   raceNumber += 1000;
 }
-//This code checks if the runner is over 18 and registered early. If both conditions are met, it adds 1000 to their raceNumber.
 
 if (age > 18 && registeredEarly) {
   console.log(`You will race at 9:30 am. Your race number is ${raceNumber}.`);
+} else if (age > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00 am. Your race number is ${raceNumber}.`);
 }
-//This code checks if the runner is over 18 and registered early.
