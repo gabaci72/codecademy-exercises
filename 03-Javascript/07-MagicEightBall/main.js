707e74c98fd2e8e326285d4e52478d6b1a42022d
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 12:08:44 +0100

    This line of code declares a constant variable userQuestion and assigns it a string containing the question.

--- 03-Javascript/07-MagicEightBall/main.js
+++ 03-Javascript/07-MagicEightBall/main.js
@@ -2,9 +2,11 @@
 
 The user will be able to input a question, then our program will output a random fortune.*/
 
-let userName = 'Seriano'; 
+let userName = "Seriano";
 // In the first line of the program, define a variable called userName that is set to an empty string.
 
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
 // This code checks if userName has a value and logs the appropriate greeting.
+const userQuestion = "Will I have a good day today?";
+// This line of code declares a constant variable userQuestion and assigns it a string containing the question.
