4a74bf75a13868bc0f7dcce4dc1c7c77b4ebf8f2
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-01 12:27:25 +0100

    We need to generate a random number between 0 and 7.

--- 03-Javascript/07-MagicEightBall/main.js
+++ 03-Javascript/07-MagicEightBall/main.js
@@ -3,10 +3,23 @@
 The user will be able to input a question, then our program will output a random fortune.*/
 
 let userName = "Seriano";
-// In the first line of the program, define a variable called userName that is set to an empty string.
+// Define userName as an empty string
 
 userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
 // This code checks if userName has a value and logs the appropriate greeting.
 const userQuestion = "Will I have a good day today?";
-// This line of code declares a constant variable userQuestion and assigns it a string containing the question.
+// Define userQuestion
+
+// Log the user's question
+console.log(`${userName} asked: ${userQuestion}`);
+//This will print the user’s name along with the question they asked.
+
+let randomNumber = Math.floor(Math.random() * 8);
+//We need to generate a random number between 0 and 7.
+
+
+
+
+
+
