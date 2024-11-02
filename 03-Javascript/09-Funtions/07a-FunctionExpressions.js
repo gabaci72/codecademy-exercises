4fe477833f965289ab9dda57e9be13f9594bba8d
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-02 16:55:22 +0100

    help us check the day of the week and if the plant needs to be watered:

---
+++ 03-Javascript/09-Funtions/07a-FunctionExpressions.js
@@ -0,0 +1,4 @@
+// Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and if the plant needs to be watered:
+const plantNeedsWater = function(day) {
+
+};
