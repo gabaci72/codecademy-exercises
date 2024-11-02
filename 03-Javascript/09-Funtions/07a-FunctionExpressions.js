b49c0c2fd6eab17edf309ceaa7ec24d33a286e27
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-02 17:07:45 +0100

    This code checks if the day is ‘Wednesday’ and returns true. If not, it returns false.

--- 03-Javascript/09-Funtions/07a-FunctionExpressions.js
+++ 03-Javascript/09-Funtions/07a-FunctionExpressions.js
@@ -1,4 +1,8 @@
-// Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and if the plant needs to be watered:
 const plantNeedsWater = function(day) {
-
-};
+    if (day === 'Wednesday') {
+      return  true;
+    } else {
+      return false;
+    }
+  };
+  
