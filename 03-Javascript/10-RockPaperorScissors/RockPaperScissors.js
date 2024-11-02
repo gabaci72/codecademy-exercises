f9a431a61d6e407cebcdd349cb807962dbfd7a2c
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-02 23:05:48 +0100

    test your function!

--- 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
+++ 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
@@ -34,30 +34,42 @@ const getComputerChoice = () => {
 
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    return 'The game is a tie!';
+    return "The game is a tie!";
   }
-  
-  if (userChoice === 'rock') {
-    if (computerChoice === 'paper') {
-      return 'The computer won!';
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer won!";
     } else {
-      return 'You won!';
+      return "You won!";
     }
   }
-  
-  if (userChoice === 'paper') {
-    if (computerChoice === 'scissors') {
-      return 'The computer won!';
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "The computer won!";
     } else {
-      return 'You won!';
+      return "You won!";
     }
   }
-  
-  if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'The computer won!';
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "The computer won!";
     } else {
-      return 'You won!';
+      return "You won!";
     }
   }
 };
+
+console.log(determineWinner("rock", "scissors")); // should print 'You won!'
+console.log(determineWinner("rock", "paper")); // should print 'The computer won!'
+console.log(determineWinner("rock", "rock")); // should print 'The game is a tie!'
+
+console.log(determineWinner("paper", "rock")); // should print 'You won!'
+console.log(determineWinner("paper", "scissors")); // should print 'The computer won!'
+console.log(determineWinner("paper", "paper")); // should print 'The game is a tie!'
+
+console.log(determineWinner("scissors", "paper")); // should print 'You won!'
+console.log(determineWinner("scissors", "rock")); // should print 'The computer won!'
+console.log(determineWinner("scissors", "scissors")); // should print 'The game is a tie!'
