66297a78c67c54a4994c7598973578dcce4c0b95
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-02 23:34:17 +0100

    Finally

--- 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
+++ 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
@@ -5,13 +5,49 @@ const getUserChoice = (userInput) => {
   if (
     userInput === "rock" ||
     userInput === "paper" ||
-    userInput === "scissors"
+    userInput === "scissors" ||
+    userInput === "bomb"
   ) {
     return userInput;
   } else {
     console.log("Error! Invalid choice.");
   }
 };
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === 'bomb') {
+    return 'You won with a secret cheat code!';
+  }
+
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  }
+  
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+  
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+  
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+};
+
 
 console.log(getUserChoice("Paper")); // should print 'paper'
 console.log(getUserChoice("fork")); // should print 'Error!' and `undefined`
@@ -32,35 +68,36 @@ const getComputerChoice = () => {
 //console.log(getComputerChoice()); // should print 'rock', 'paper', or 'scissors'
 //console.log(getComputerChoice()); // should print 'rock', 'paper', or 'scissors'
 
-const determineWinner = (userChoice, computerChoice) => {
-  if (userChoice === computerChoice) {
-    return "The game is a tie!";
-  }
-
-  if (userChoice === "rock") {
-    if (computerChoice === "paper") {
-      return "The computer won!";
-    } else {
-      return "You won!";
-    }
-  }
-
-  if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-      return "The computer won!";
-    } else {
-      return "You won!";
-    }
-  }
-
-  if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-      return "The computer won!";
-    } else {
-      return "You won!";
-    }
-  }
-};
+// Remove the second declaration of determineWinner
+// const determineWinner = (userChoice, computerChoice) => {
+//   if (userChoice === computerChoice) {
+//     return "The game is a tie!";
+//   }
+//
+//   if (userChoice === "rock") {
+//     if (computerChoice === "paper") {
+//       return "The computer won!";
+//     } else {
+//       return "You won!";
+//     }
+//   }
+//
+//   if (userChoice === "paper") {
+//     if (computerChoice === "scissors") {
+//       return "The computer won!";
+//     } else {
+//       return "You won!";
+//     }
+//   }
+//
+//   if (userChoice === "scissors") {
+//     if (computerChoice === "rock") {
+//       return "The computer won!";
+//     } else {
+//       return "You won!";
+//     }
+//   }
+// };
 
 console.log(determineWinner("rock", "scissors")); // should print 'You won!'
 console.log(determineWinner("rock", "paper")); // should print 'The computer won!'
