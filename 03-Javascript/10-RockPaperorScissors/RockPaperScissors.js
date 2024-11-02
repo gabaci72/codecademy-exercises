7d7272fe780d0124bf1bb7d23a89c5ea3b34c59e
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-02 18:31:44 +0100

    This function randomly selects one of the three choices for the computer.

--- 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
+++ 03-Javascript/10-RockPaperorScissors/RockPaperScissors.js
@@ -12,3 +12,14 @@ const getUserChoice = userInput => {
 console.log(getUserChoice('Paper')); // should print 'paper'
 console.log(getUserChoice('fork')); // should print 'Error!' and `undefined`
 
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
