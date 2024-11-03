0a465431426d274c8f9f0b00f3a35814d7d92b6a
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-03 11:17:58 +0100

    Test I two new functions by calling them and printing the results to the console.

--- 03-Javascript/11-SleepDebtCalculator/SleepDebtCalculator.js
+++ 03-Javascript/11-SleepDebtCalculator/SleepDebtCalculator.js
@@ -3,47 +3,49 @@
 Finally, it will calculate, in hours, how far you are from your weekly sleep goal.*/
 
 const getSleepHours = (day) => {
-    switch (day.toLowerCase()) {
-      case 'monday':
-        return 8;
-      case 'tuesday':
-        return 7;
-      case 'wednesday':
-        return 6;
-      case 'thursday':
-        return 5;
-      case 'friday':
-        return 8;
-      case 'saturday':
-        return 9;
-      case 'sunday':
-        return 7;
-      default:
-        return 'Invalid day';
-    }
-  };
-  
-  console.log(getSleepHours('monday'));   // should print 8
-  console.log(getSleepHours('tuesday'));  // should print 7
-  console.log(getSleepHours('wednesday'));// should print 6
-  console.log(getSleepHours('thursday')); // should print 5
-  console.log(getSleepHours('friday'));   // should print 8
-  console.log(getSleepHours('saturday')); // should print 9
-  console.log(getSleepHours('sunday'));   // should print 7
-  
-  const getActualSleepHours = () => 
-    getSleepHours('monday') +
-    getSleepHours('tuesday') +
-    getSleepHours('wednesday') +
-    getSleepHours('thursday') +
-    getSleepHours('friday') +
-    getSleepHours('saturday') +
-    getSleepHours('sunday');
-  
-  
-  const getIdealSleepHours = () => {
-    const idealHours = 8; // Set this to your ideal hours per night
-    return idealHours * 7;
-  };
-  
-  
+  switch (day.toLowerCase()) {
+    case 'monday':
+      return 8;
+    case 'tuesday':
+      return 7;
+    case 'wednesday':
+      return 6;
+    case 'thursday':
+      return 5;
+    case 'friday':
+      return 8;
+    case 'saturday':
+      return 9;
+    case 'sunday':
+      return 7;
+    default:
+      return 'Invalid day';
+  }
+};
+
+console.log(getSleepHours('monday'));   // should print 8
+console.log(getSleepHours('tuesday'));  // should print 7
+console.log(getSleepHours('wednesday'));// should print 6
+console.log(getSleepHours('thursday')); // should print 5
+console.log(getSleepHours('friday'));   // should print 8
+console.log(getSleepHours('saturday')); // should print 9
+console.log(getSleepHours('sunday'));   // should print 7
+
+const getActualSleepHours = () => 
+  getSleepHours('monday') +
+  getSleepHours('tuesday') +
+  getSleepHours('wednesday') +
+  getSleepHours('thursday') +
+  getSleepHours('friday') +
+  getSleepHours('saturday') +
+  getSleepHours('sunday');
+
+
+const getIdealSleepHours = () => {
+  const idealHours = 8; // Set this to your ideal hours per night
+  return idealHours * 7;
+};
+
+console.log(getActualSleepHours()); // should print the sum of all sleep hours in the week
+console.log(getIdealSleepHours());  // if idealHours is 8, should print 56
+
