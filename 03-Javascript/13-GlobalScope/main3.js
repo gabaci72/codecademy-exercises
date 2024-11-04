e3f52e8c4707ad32dc88bdd2817812a73bd9a67b
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-04 10:12:16 +0100

    When you run this code, it will log 'Northern Lights' inside the if block and 'Moonlight' outside the if block, demonstrating block scope.

--- 03-Javascript/13-GlobalScope/main3.js
+++ 03-Javascript/13-GlobalScope/main3.js
@@ -4,9 +4,11 @@ const logVisibleLightWaves = () => {
 //1.create an if statement that checks if the region is the 'The Arctic'.
   if (region === 'The Arctic') {
    let lightWaves = 'Northern Lights'; // 2.New variable inside the if block
+   console.log(lightWaves);// 3.Logs 'Northern Lights'
   }
 
-  console.log(lightWaves);
+  console.log(lightWaves);// Logs 'Moonlight'
 };
 
 logVisibleLightWaves();
+//When you run this code, it will log 'Northern Lights' inside the if block and 'Moonlight' outside the if block, demonstrating block scope.
