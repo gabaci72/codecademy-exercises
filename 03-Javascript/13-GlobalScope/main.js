0493b648d14e9c12e68aaf4ab36756856e7b4f31
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-04 07:54:01 +0100

    I write comments

--- 03-Javascript/13-GlobalScope/main.js
+++ 03-Javascript/13-GlobalScope/main.js
@@ -1,3 +1,8 @@
+//t the top of main.js, write three global variables:
+//Name the first variable satellite and set it equal to 'The Moon'.
 const satellite = 'The Moon';
+//Name the second variable galaxy and set it equal to 'The Milky Way'.
 const galaxy = 'The Milky Way';
+//Name the third variable stars and set it equal to 'North Star'.
 const stars = 'North Star';
+
