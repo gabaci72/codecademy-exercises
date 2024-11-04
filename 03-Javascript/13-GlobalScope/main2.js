0503d317d372bc7e4cb0b1f27b17cf3258c90c35
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-04 08:33:07 +0100

    I adding a console.log() statement that will log the value of the lightWaves variable when the function runs

--- 03-Javascript/13-GlobalScope/main2.js
+++ 03-Javascript/13-GlobalScope/main2.js
@@ -1,4 +1,6 @@
 //define a function
 const logVisibleLightWaves = () => {
-  const lightWaves = 'Moonlight';//using const, create a variable lightWaves and set it equal to 'Moonlight'
+  const lightWaves = 'Moonlight';//2.using const, create a variable lightWaves and set it equal to 'Moonlight'
+  console.log(lightWaves);//3.add a console.log() statement that will log the value of the lightWaves variable when the function runs.
 };
+
