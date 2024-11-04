8b0752d3ad61e4a795079f78f6bc9be30bb1539d
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-04 14:37:48 +0100

    To avoid the ReferenceError, declare days within the getTrainingDays function, before the if statement.

--- 03-Javascript/14-TrainingDays/trainingDays.js
+++ 03-Javascript/14-TrainingDays/trainingDays.js
@@ -1,10 +1,5 @@
-/*I declared let days; outside of the if blocks and then assigned values based on the event type. 
-#This allows to return the days variable without encountering a ReferenceError. */
-
-// The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
-
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3); // Move random inside the function
   if (random === 0) {
     return 'Marathon';
   } else if (random === 1) {
@@ -14,34 +9,30 @@ const getRandEvent = () => {
   }
 };
 
-// The scope of `days` is too tight 
 const getTrainingDays = event => {
-  let days; // 1.Declare days here
+  let days; // Declare days here
   if (event === 'Marathon') {
     days = 50; // Assign value to days
   } else if (event === 'Triathlon') {
     days = 100; // Assign value to days
   } else if (event === 'Pentathlon') {
-    days = 200; // Assign value to days
+    days = 200; // Assign value to daysgit
   }
-
   return days;
 };
 
-// The scope of `name` is too tight 
-const logEvent = event => {
-  const name = 'Nala';
+const name = 'Nala'; // Move name to global scope
+
+const logEvent = (name, event) => {
   console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = days => {
-  const name = 'Nala';
+const logTime = (name, days) => {
   console.log(`${name}'s time to train is: ${days} days`);
 };
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
 
-logEvent(event);
-logTime(days);
+logEvent(name, event);
+logTime(name, days);
