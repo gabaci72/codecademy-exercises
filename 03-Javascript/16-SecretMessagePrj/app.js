53ab7254a994cd2fe65fb5e97e3ecb6d8401a571
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-14 00:26:14 +0100

    updating

--- 03-Javascript/16-SecretMessagePrj/app.js
+++ 03-Javascript/16-SecretMessagePrj/app.js
@@ -1,1 +1,22 @@
+// Project: Using array methods, you will transform an array of strings into a secret message!
+
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+/* 2. Great! You can check your work by logging the .length of the array.
+
+At this point, the length should be 1 less than the original length.*/
+// Log the length of the array to check
+console.log(secretMessage.length); // This should output: 23
+
+/* 3.Use an array method to add the words to and Program as separate strings to the end of the secretMessage array.*/
+// Add 'to' and 'Program' to the end of the secretMessage array
+secretMessage.push('to', 'Program');
+
+
+
+
+
+
+
