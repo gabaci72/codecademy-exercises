566af18b2f2c547359563900262cb9e9910983fa
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-14 12:22:47 +0100

    Repeating Tasks Manually

---
+++ 03-Javascript/17-LoopsExercises/01-RepeatingTasksManually.js
@@ -0,0 +1,10 @@
+// Repeating Tasks Manually
+
+/* 1.Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.*/
+const vacationSpots = ['Paris', 'London', 'New York'];
+
+/* 2.Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately.*/
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
