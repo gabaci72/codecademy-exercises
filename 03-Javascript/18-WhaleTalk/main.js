c72379d66a6e4a3c28795e619b92ea7f8a1b76b3
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-21 01:26:02 +0100

    create 2nested loops

---
+++ 03-Javascript/18-WhaleTalk/main.js
@@ -0,0 +1,14 @@
+const input = 'Hello, how are you today?';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+}
+
+console.log(resultArray);
+
