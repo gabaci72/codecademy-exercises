064c46f20186ea70522b8d141ca34e4de4f797c4
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-21 12:58:38 +0100

    Im make a nested loops

--- 03-Javascript/18-WhaleTalk/main.js
+++ 03-Javascript/18-WhaleTalk/main.js
@@ -11,6 +11,29 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 This will serve as a place to store the vowels from the input string.*/
 let resultArray = [];
 
+/* 4.Create a loop to iterate through each letter of the input variable text.
+ In a later step, we will compare each letter with our vowels array.*/
 
+// for (let i = 0; i < input.length; i++) {
+//   // Code to process each letter will go here
+// }
+
+/* To check your work, log the iterator numbered position inside the for
+ loop and run your code. This should count the number of characters in your input string.
+
+Comment out this code when you’re ready to move on.*/
+// for (let i = 0; i < input.length; i++) {
+//   // console.log('i is ' + i);
+// }
+
+/* 6.Create a nested for loop inside of the for loop you just wrote. Make the inner loop 
+iterate through the vowels array each time the outer loop runs.
+
+This will enable you to check each letter of input against all the vowels elements during each iteration.*/
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    // Code to compare input[i] with vowels[j] will go here
+  }
+}
 
 
