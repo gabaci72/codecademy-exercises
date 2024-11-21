598e527b2528d1dfe3ec4a91a65122fa0d633e5b
gabaci72 <76642629+gabaci72@users.noreply.github.com>
2024-11-21 11:33:47 +0100

    I started from the beginning again

--- 03-Javascript/18-WhaleTalk/main.js
+++ 03-Javascript/18-WhaleTalk/main.js
@@ -1,14 +1,16 @@
+/* 1.Create a variable named input that is equal to any phrase you’d like. 
+This variable will contain the text you want to translate into “whale talk”.*/
 const input = 'Hello, how are you today?';
+
+/* 2.Whales only speak with the vowels, “a”, “e”, “i”, “o”, and “u”. 
+Using these lowercase letters, create an array named vowels. 
+This array will not be updated so be sure to choose the appropriate declaration keyword.*/
 const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+/* 3.Create a variable named resultArray and set it equal to an empty array: []. 
+This will serve as a place to store the vowels from the input string.*/
 let resultArray = [];
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < vowels.length; j++) {
-    if (input[i] === vowels[j]) {
-      resultArray.push(input[i]);
-    }
-  }
-}
 
-console.log(resultArray);
+
 
