8cf6cad366d284754170dab23c1af9cede676f91
gabaci72 <serianonim@gmail.com>
2024-11-25 15:44:00 +0100

    map method exercises

---
+++ 03-Javascript/19-Iterators/05a-ThemapMethod.js
@@ -0,0 +1,6 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
